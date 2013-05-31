734e6a4332de4ac6870e70159e7b0993c188cf3b
arya <sedekahin@gmail.com>
2013-05-31 10:53:44 +0700

    the master

---
+++ wp-content/themes/simplefast-responsive/footer.php
@@ -0,0 +1,11 @@
+<div style="clear: both"></div>
+</div>
+<footer id="footer">
+<?php $pages = wp_list_pages('depth=1&title_li=&echo=0');
+$pages2 = preg_split('/(<li[^>]*>)/' ,$pages);foreach($pages2 as $var){
+echo str_replace('</li>', '', $var);}?><br/>
+Copyright &#169; 2012  <a href="<?php echo home_url() ; ?>"><?php bloginfo('name'); ?></a>, All trademarks are the property of the respective trademark owners. <br/>
+</footer>
+</div>
+</body>
+</html>
